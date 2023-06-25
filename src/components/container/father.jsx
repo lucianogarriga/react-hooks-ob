6c1580177c9363589b7951be32448fafcb228fd8
Luciano Garriga <lucianomgarriga@gmail.com>
2023-06-24 22:17:07 -0300

    Events - Props - State

--- src/components/container/father.jsx
+++ src/components/container/father.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Child from '../pure/child';
 
 const Father = () => {
+
+    const [name, setName] = useState('Carlos');
 
     function showMessage(text){
         alert(`Mensaje recibido desde el hijo: ${text}`)
     }
 
+    function updateName(newName){
+        setName(newName)
+        console.log(`El nuevo nombre es: ${newName}`)
+    }
+
     return (
         <div>
-            <Child name="Child" send={showMessage}></Child>
+            <Child name={name} send={showMessage} update={updateName}></Child>
         </div>
     );
 }
--- src/components/pure/child.jsx
+++ src/components/pure/child.jsx
@@ -1,8 +1,9 @@
 import React, {useRef} from "react";
 
-const Child = ({ name, send }) => {
+const Child = ({ name, send, update }) => {
 
   const msgRef = useRef('')
+  const nameRef = useRef('')
 
   function pressButton() {
     const text = msgRef.current.value;
@@ -17,25 +18,38 @@ const Child = ({ name, send }) => {
     console.log("Input Focused");
   };
 
+  function submitName(e) {
+    e.preventDefault();
+    update(nameRef.current.value)
+  }
+
   return (
     <div>
       <p
         onMouseOver={() => {
-          console.log("OnMouseOver");
+          console.log( `Hello ${name}`);
         }}
       >
-        {name} Component
+        {name}
       </p>
       <button onClick={pressButton}>Button 1</button>
       <button onClick={() => pressButtonParams("Button 2")}>Button 2</button>
       <input
-        placeholder="Insert a text"
+        placeholder = 'Send a text to your father'
         onFocus={inputFocused}
         onChange={(e) => console.log("Input: ", e.target.value)}
         onCopy={() => alert("Text copied")}
         ref={msgRef}
       />
-      <button onClick={( ) => send('Hello father')}>Send msg to father</button>
+      <button onClick={( ) => send(msgRef.current.value)}>Send msg to father</button>
+
+      <div style={{marginTop:"20px"}}>
+        <form onSubmit={submitName}>
+          <input ref={nameRef} placeholder="New Name"/>
+          <button type="submit">Update Name</button>
+
+        </form>
+      </div>
     </div>
   );
 };
