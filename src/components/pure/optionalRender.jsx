2ee84ff8e26677306b37f8549fbfdeccf499bd99
Luciano Garriga <lucianomgarriga@gmail.com>
2023-07-09 11:37:17 -0300

    Optional render - Login - Logout - New msg

--- src/components/pure/optionalRender.jsx
+++ src/components/pure/optionalRender.jsx
@@ -12,28 +12,41 @@ const LogoutButton = ({ logoutAction }) => {
 
 const OptionalRender = () => {
   const [access, setAccess] = useState(true);
-
-  // const updateAccess = () => {
-  //     setAccess(!access)
-  // }
+  // Msg sin leer del usuario
+  const [nMessages, setNMessages] = useState(0);
 
   const loginAction = () => {
     setAccess(true);
   };
-
   const logoutAction = () => {
     setAccess(false);
   };
 
   let optionalButton;
-
-  if (access) {
-    optionalButton = <LogoutButton logoutAction={logoutAction} />;
-  } else {
-    optionalButton = <LoginButton loginAction={loginAction} />;
+  // if (access) {
+  //   optionalButton = <LogoutButton logoutAction={logoutAction} />;
+  // } else {
+  //   optionalButton = <LoginButton loginAction={loginAction} />;
+  // }
+  {
+    access
+      ? (optionalButton = <LogoutButton logoutAction={logoutAction} />)
+      : (optionalButton = <LoginButton loginAction={loginAction} />);
+  }
+ 
+  // Unread msg
+  let addMessages = ( ) => {
+    setNMessages(nMessages + 1)
   }
 
-  return <div>{optionalButton}</div>;
+  return (
+      <div>
+        {optionalButton}
+        {nMessages > 0 && <p>You have {nMessages} new messages...</p>}
+        {nMessages == 0 && <p>You don't have new messages...</p>}
+        <button onClick={addMessages}>Add new messages</button>
+      </div>
+  )
 };
 
 export default OptionalRender;
