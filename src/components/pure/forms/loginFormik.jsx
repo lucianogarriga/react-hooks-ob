2f0426f4afd006f54c4f3faf1a61c877e984fc8c
Luciano Garriga <lucianomgarriga@gmail.com>
2023-07-10 23:20:59 -0300

    Error Message - Props - Children

--- src/components/pure/forms/loginFormik.jsx
+++ src/components/pure/forms/loginFormik.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Formik, Field, Form } from "formik";
+import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
 const loginSchema = Yup.object().shape({
@@ -13,7 +13,7 @@ const LoginFormik = () => {
   const initialCredentials = {
     email: "",
     password: "",
-  }; 
+  };
 
   return (
     <div>
@@ -23,56 +23,57 @@ const LoginFormik = () => {
         // Yup Validation Schema
         validationSchema={loginSchema}
         onSubmit={async (values) => {
-          await new Promise(r => setTimeout(r, 2000))
+          await new Promise((r) => setTimeout(r, 2000));
           alert(JSON.stringify(values, null, 2));
           // We save the data in local storage of browser
-          localStorage.setItem('credentials', values)
+          localStorage.setItem("credentials", values);
         }}
       >
         {/* Obtain Props from Formik */}
 
-        {(props) => {
-          const {
-            values, // obtain Form values
-            touched, // if user touched any field
-            errors, // obtain error inside a field or form
-            isSubmitting, // if was send 100% or is sending
-            handleChange, // if exists any change in a field to control it
-            handleBlur, // if exists any focus change
-          } = props;
-
-          return (
-            <Form>
-              <label htmlFor="email">Email</label>
-              <Field id="email" type="email" name="email" placeholder="example@email.com" />
-              {/* Email errors */}
-              { 
-                errors.email && touched.email && 
-                (
-                  <div className="error">
-                    <p>{errors.email}</p>
-                  </div>
-                )
-              }
-              <label htmlFor="password">Password</label>
-              <Field id="password" type="password" name="password" placeholder="password1234" />
-              {/* Password errors */}
-              { 
-                errors.password && touched.password && 
-                (
-                  <div className="error">
-                    <p>{errors.password}</p>
-                  </div>
-                )
-              }
-              <button type="submit">Login</button>
-              {isSubmitting 
-              ? (<p>Login your credentials...</p>)
-              : null
-              }
-            </Form>
-          );
-        }}
+        {({
+          values,
+          errors,
+          touched,
+          isSubmitting,
+          handleChange,
+          handleBlur,
+        }) => (
+          <Form>
+            <label htmlFor="email" className="mt-3">
+              Email
+            </label>
+            <Field
+              id="email"
+              type="email"
+              name="email"
+              placeholder="example@email.com"
+            />
+            {/* Email errors */}
+            {errors.email && touched.email && ( 
+                <ErrorMessage component="div" name="email" /> 
+            )}
+            <br/>
+            <label htmlFor="password" className="mt-3">
+              Password
+            </label>
+            <Field
+              id="password"
+              type="password"
+              name="password"
+              placeholder="password1234"
+            />
+            {/* Password errors */}
+            {errors.password && touched.password && ( 
+                <ErrorMessage name="password" component="div" /> 
+            )}
+            <br/>
+            <button type="submit" className="m-3">
+              Login
+            </button>
+            {isSubmitting ? <p>Login your credentials...</p> : null}
+          </Form>
+        )}
       </Formik>
     </div>
   );
