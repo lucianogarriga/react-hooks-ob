b1d775f23fb778574a233891abd7d082a56eccfa
Luciano Garriga <lucianomgarriga@gmail.com>
2023-07-21 11:58:00 -0300

    New buttons and examples with Async Functions

--- src/components/pure/AsyncExample.jsx
+++ src/components/pure/AsyncExample.jsx
@@ -17,11 +17,40 @@ const AsyncExample = () => {
   }
 
   function obtainPromiseNumber(){
-    generatePromiseNumber().then((res) => alert(`Response of PromiseNum: ${res}`))
+    generatePromiseNumber()
+    .then((res) => alert(`Response of PromiseNum: ${res}`))
+    .catch((error) => alert(`Something went wrong: ${error}`))
   }
 
   async function saveSessionStorage(key, value) {
     await sessionStorage.setItem(key, value);
+
+    return Promise.resolve(sessionStorage.getItem(key,value))
+  }
+
+  function showStorage(){
+    saveSessionStorage('name', 'Luciano')
+    .then((res) => {
+      let value = res;
+      alert(`Saved name: ${value}`)
+    })
+    .catch((error) => {
+      alert(`Something went wrong: ${error}`)
+    })
+    .finally(() => alert('Name saved and retrieved successfully'))
+  }
+
+  async function obtainMessage(){
+    let promise = new Promise((res, rej) => {
+      setTimeout(() => {
+        res('Hello world')
+      }, 2000);
+    })
+
+    // Now we show the result
+    let message = await promise;
+
+    alert(`The message is: ${message}`);
   }
 
   return (
@@ -29,6 +58,8 @@ const AsyncExample = () => {
       <h1>Async Example</h1>
       <button onClick={obtainNumber}>Obtener numero</button>
       <button onClick={obtainPromiseNumber}>Obtener Promesa</button>
+      <button onClick={showStorage}>Save Name</button>
+      <button onClick={obtainMessage}>Show message</button>
     </div>
   );
 };
